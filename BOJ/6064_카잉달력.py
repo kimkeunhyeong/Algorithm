6953f87cb06e16e299eb331b30cd40fdfaa57378
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-08-22 18:06:16 +0900

    [EDIT] BOJ-6064, T : 800 ms, M : 33376 KB

--- BOJ/6064_카잉달력.py
+++ BOJ/6064_카잉달력.py
@@ -4,14 +4,16 @@ from math import lcm
 def get_result(M, N, x, y):
     year = x
     MAX_year = lcm(M, N)
+    if y == N:
+        # y를 0으로 만들면 반복문 안에서 추가로 검사할 필요가 없음.
+        # y_prime이 0일 때의 코드 삭제 후 실행시간 64ms(7%) 추가 감소.
+        y = 0
     while year <= MAX_year:
-        x_prime, y_prime = year % M, year % N
-        if not x_prime:
-            x_prime = M
-        if not y_prime:
-            y_prime = N
+        # 시작을 x부터 시작하고, M만큼 증가하며 검사하니 x를 검사할 필요가 없음.
+        # -> x_prime, x 검사 부분 삭제 후 실행시간 528ms(38%) 감소.
+        y_prime = year % N
 
-        if x == x_prime and y == y_prime:
+        if y == y_prime:
             return year
         year += M
 
