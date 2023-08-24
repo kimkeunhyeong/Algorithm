15533ec162635fe1eee58b9614415e1f32db11b7
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-08-24 18:09:44 +0900

    BOJ-20529, T : 480 ms, M : 38708 KB

--- BOJ/20529_가장가까운세사람의심리적거리.py
+++ BOJ/20529_가장가까운세사람의심리적거리.py
@@ -1,25 +1,27 @@
 from sys import stdin
-from collections import Counter
-from math import comb
 from itertools import combinations as combi
+
+def check(A, B):
+    count = 0
+    for i in range(4):
+        if A[i] != B[i]:
+            count += 1
+
+    return count
 
 input = stdin.readline
 
 TC = int(input())
 
 for _ in range(TC):
-    input()
-    students = Counter(input().rstrip().split())
-    distance = []
-    for i in students.values():
-        if i > 1:
-            distance += [0] * comb(i, 2)
-    for A, B in combi(students.keys(), 2):
-        temp_dist = 0
-        for i in range(4):
-            temp_dist += (A[i] != B[i])
-
-        distance += [temp_dist] * students[A] * students[B]
+    num_student = int(input())
+    students = input().rstrip().split()
+    if num_student > 32: # 16개의 성격이 2명씩 있다고 할때, 최대. 이후는 무조건 한 성격이 3명인 경우가 있음
+        print(0)
+    else:
+        distance = 999999
+        for A, B, C in combi(students, 3):
+            distance = min(distance, check(A, B) + check(A, C) + check(B, C))
+            if distance == 0:
+                break
         print(distance)
-    distance.sort()
-    print(sum(distance[:3]))
