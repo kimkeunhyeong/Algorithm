50eff2d457823ada1fe96be5bc22b3b807bb64b7
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-08-29 21:19:55 +0900

    [EDIT] remove function BOJ-9019, fail : time limit exceeded

--- BOJ/9019_DSLR.py
+++ BOJ/9019_DSLR.py
@@ -1,21 +1,6 @@
 from sys import stdin
 from collections import deque
 
-def operation(number : int, op_type : str) -> int:
-    match op_type:
-        case "D":
-            return (number * 2) % 10000
-        case "S":
-            return number - 1 if number > 0 else 9999
-        case "L":
-            digit_one = number // 1000
-            return (number % 1000) * 10 + digit_one
-        case "R":
-            digit_four = number % 10
-            return (number // 10) + 1000 * digit_four
-        case _:
-            raise TypeError
-        
 input = stdin.readline
 TC = int(input())
 
@@ -25,15 +10,29 @@ for _ in range(TC):
     DP = [False] * 10000
 
     Queue = deque([(current, "")])
-    
+    DP[current] = True # "" 인 경우 if문에서 False 취급되므로
+
     while Queue:
         number, string = Queue.popleft()
-        current_length = len(string)
+        if number == target:
+            break
 
-        for op in ["D", "S", "L", "R"]:
-            next_number = operation(number, op)
-            if not DP[next_number]:
-                DP[next_number] = string + op
-                Queue.append((next_number, string + op))
+        num_D = (number * 2) % 10000
+        num_S = number - 1 if number > 0 else 9999
+        num_L = (number % 1000) * 10 + number // 1000
+        num_R = (number // 10) + 1000 * number % 10
+
+        if not DP[num_D]:
+            DP[num_D] = string + "D"
+            Queue.append((num_D, string + "D"))
+        if not DP[num_S]:
+            DP[num_S] = string + "S"
+            Queue.append((num_S, string + "S"))
+        if not DP[num_L]:
+            DP[num_L] = string + "L"
+            Queue.append((num_L, string + "L"))
+        if not DP[num_R]:
+            DP[num_R] = string + "R"
+            Queue.append((num_R, string + "R"))
 
     print(DP[target])
