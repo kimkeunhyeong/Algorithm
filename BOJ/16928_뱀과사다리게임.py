20df252904b1d9f2840f5de32bfcd1759df9e9bf
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-05 16:31:07 +0900

    BOJ-16928, fail : wrong answer

---
+++ BOJ/16928_뱀과사다리게임.py
@@ -0,0 +1,33 @@
+from sys import stdin
+from math import inf
+from collections import deque
+
+input = stdin.readline
+
+ladder_cnt, snake_cnt = map(int, input().split())
+board_map = [0] * 101
+board_cnt = [inf] * 101
+
+for i in range(ladder_cnt + snake_cnt):
+    start, end = map(int, input().split())
+    board_map[start] = end
+
+Queue = deque([1])
+board_cnt[1] = 0
+
+while Queue:
+    current = Queue.popleft()
+    if current == 100:
+        print(board_cnt[100])
+        break
+    move_cnt = board_cnt[current]
+
+    for i in range(1, 7):
+        if (next_ := current + i) <= 100 and board_cnt[next_] > move_cnt + 1:
+            if special := board_map[next_]:
+                board_cnt[next_] = move_cnt + 1 # next 방문처리 & 사다리 or 뱀으로 이동
+                board_cnt[special] = move_cnt + 1
+                Queue.append(special)
+            else:
+                board_cnt[next_] = move_cnt + 1
+                Queue.append(next_)
