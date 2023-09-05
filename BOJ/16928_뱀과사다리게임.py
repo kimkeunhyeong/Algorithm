4a81787683ade0b9d81b1442703c1f0c0f90e63f
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-05 19:19:07 +0900

    [EDIT] change from deque to list  BOJ-16928, T : 40 ~ 48 ms, M : 31256 KB

--- BOJ/16928_뱀과사다리게임.py
+++ BOJ/16928_뱀과사다리게임.py
@@ -1,34 +1,28 @@
 from sys import stdin
-from math import inf
-from collections import deque
 
 input = stdin.readline
 
 ladder_cnt, snake_cnt = map(int, input().split())
-board_map = [0] * 101
-board_cnt = [inf] * 101
+board_map = [i for i in range(101)]
+board_cnt = [0] * 101
 
 for i in range(ladder_cnt + snake_cnt):
     start, end = map(int, input().split())
     board_map[start] = end
 
-Queue = deque([1])
-board_cnt[1] = 0
+Queue = [1]
 
 while Queue:
-    current = Queue.popleft()
-    if current == 100:
-        print(board_cnt[100])
-        break
-    move_cnt = board_cnt[current]
+    current = Queue.pop(0)
+    # O(N)에서 N이 충분히 작은 경우, deque보다 list.pop(0)이 빠를 수도 있음.
+    move_cnt = board_cnt[current] + 1
+    start = current + 1
+    end = current + 7 if current <= 94 else 101 # 범위 검사는 한번만 하도록 변경
 
-    for i in range(1, 7):
-        if (next_ := current + i) <= 100 and board_cnt[next_] > move_cnt + 1:
-            if special := board_map[next_]:
-                board_cnt[next_] = move_cnt + 1 # next 방문처리 & 사다리 or 뱀으로 이동
-                if board_cnt[special] > move_cnt + 1: # 여기서 방문 확인해줘야 함..
-                    board_cnt[special] = move_cnt + 1
-                    Queue.append(special)
-            else:
-                board_cnt[next_] = move_cnt + 1
-                Queue.append(next_)
+    for i in range(start, end):
+        next_ = board_map[i] # 사다리 or 뱀인 경우 해당 칸으로 이동하고
+        if not board_cnt[next_]:
+            board_cnt[next_] = move_cnt # 방문처리
+            Queue.append(next_)
+
+print(board_cnt[100])
