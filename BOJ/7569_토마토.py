0b6a8d0febedc10eb01ab48873e8ea1d2f122f86
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-06 21:36:55 +0900

    BOJ-7569, T : 1996 ms, M : 48504 KB

---
+++ BOJ/7569_토마토.py
@@ -0,0 +1,66 @@
+from sys import stdin
+from collections import deque
+
+def BFS(g):
+    global days
+    Queue = deque()
+    for i in g:
+        Queue.append(i)
+    
+    while Queue:
+        days += 1
+        for _ in range(len(Queue)):
+            z, y, x = Queue.popleft()
+            for next_z, next_y, next_x in [(z, y, x + 1), (z, y, x - 1),
+                                           (z, y + 1, x), (z, y - 1, x),
+                                           (z + 1, y, x), (z - 1, y, x)]:
+                if 0 <= next_z < height and 0 <= next_y < length and 0 <= next_x < width and \
+                    tomato[next_z][next_y][next_x] != -1:
+                    if tomato[next_z][next_y][next_x] == 0:
+                        tomato[next_z][next_y][next_x] += 1
+                        Queue.append((next_z, next_y, next_x))
+    return days
+
+input = stdin.readline
+
+width, length, height = map(int, input().split())
+tomato = []
+for i in range(height):
+    floor = []
+    for j in range(length):
+        floor.append(list(map(int, input().split())))
+    tomato.append(floor)
+
+graph = []
+not_ripen = 0
+days = -1 # BFS 마지막 loop는 모두 Visited이므로 1을 뺴줘야 함
+
+for i in range(height):
+    for j in range(length):
+        for k in range(width):
+            if not tomato[i][j][k]:
+                not_ripen += 1
+            if tomato[i][j][k] == 1:
+                graph.append((i,j,k))
+                tomato[i][j][k] += 1
+
+if not_ripen:
+    BFS(graph)
+    # BFS 실행 후 안익은 토마토 검색
+    for i in range(height):
+        for j in range(length):
+            for k in range(width):
+                if not tomato[i][j][k]:
+                    print(-1)
+                    break
+            else:
+                continue
+            break
+        else:
+            continue
+        break
+    else:
+        print(days)
+# 이미 익은 토마토만 있는 경우, 0 출력
+else:
+    print(0)
