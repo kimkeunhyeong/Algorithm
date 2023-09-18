6f4fcd0df37b2267b74929045705fe746e6918e0
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-19 03:31:19 +0900

    change initial Visited value to -1 BOJ-13549, T : 88 ms, M : 34128 KB

--- BOJ/13549_숨바꼭질3.py
+++ BOJ/13549_숨바꼭질3.py
@@ -2,8 +2,9 @@ from collections import deque
 
 def BFS(start, target):
     Queue = deque()
-    Visited = [0] * 100001
+    Visited = [-1] * 100001
     Queue.append(start)
+    Visited[start] = 0
 
     while Queue:
         check = Queue.popleft()
@@ -11,13 +12,15 @@ def BFS(start, target):
         if check == target:
             break
 
-        if check < 50001 and not Visited[check * 2]:
-            Visited[check * 2] = curr_time - 1
+        # Visited == False로 검사한 부분에서, 최초 시작점의 방문처리가 불가능해서 오류 발생함.
+        # -> 시작값을 0이 아닌 -1로 설정하여 방문여부를 확인
+        if check < 50001 and Visited[check * 2] == -1:
+            Visited[check * 2] = Visited[check]
             Queue.append(check * 2)
-        if check > 0 and not Visited[check - 1]:
+        if check > 0 and Visited[check - 1] == -1:
             Visited[check - 1] = curr_time
             Queue.append(check - 1)
-        if check < 100000 and not Visited[check + 1]:
+        if check < 100000 and Visited[check + 1] == -1:
             Visited[check + 1] = curr_time
             Queue.append(check + 1)
 
