4ba03a5bcc994140a659ce7a5600a93b20582a52
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-19 03:15:34 +0900

    BOJ-13549, fail : IndexError, range check missing

---
+++ BOJ/13549_숨바꼭질3.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def BFS(start, target):
+    Queue = deque([start * 2, start - 1, start + 1])
+    Visited = [0] * 100001
+    Visited[start - 1] = Visited[start + 1] = 1
+
+    while Queue:
+        check = Queue.popleft()
+        curr_time = Visited[check] + 1
+        if check == target:
+            break
+
+        if check < 50001 and not Visited[check * 2]:
+            Visited[check * 2] = curr_time - 1
+            Queue.append(check * 2)
+        if check > 0 and not Visited[check - 1]:
+            Visited[check - 1] = curr_time
+            Queue.append(check - 1)
+        if check < 100000 and not Visited[check + 1]:
+            Visited[check + 1] = curr_time
+            Queue.append(check + 1)
+
+    print(Visited[target])
+
+start, target = map(int, input().split())
+
+if start >= target:
+    print(abs(target - start))
+else:
+    BFS(start, target)
