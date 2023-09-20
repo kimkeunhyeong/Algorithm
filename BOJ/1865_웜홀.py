064371c1f88bea4893ed47c51713fe3926471e21
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-20 18:25:11 +0900

    BOJ-1865, T : 1196 ms, M : 33376 KB

--- BOJ/1865_웜홀.py
+++ BOJ/1865_웜홀.py
@@ -2,19 +2,16 @@ from sys import stdin
 from math import inf
 
 def Bell(g, start):
-    Visited = [inf] * (point_cnt + 1)
-    Visited[start] = 0
+    distance = [1e9] * (point_cnt + 1)
+    distance[start] = 0
     
-    for repeat in range(point_cnt - 1):
+    for repeat in range(1, point_cnt + 1):
         for point in range(1, point_cnt + 1):
-            for connected in g[point]:
-                if Visited[connected[0]] > Visited[point] + connected[1]:
-                    Visited[connected[0]] = Visited[point] + connected[1]
-    
-    for point in range(1, point_cnt + 1):
-        for connected in g[point]:
-            if Visited[connected[0]] > Visited[point] + connected[1]:
-                return True
+            for connected, time in g[point]:
+                if distance[connected] > distance[point] + time:
+                    distance[connected] = distance[point] + time
+                    if repeat == point_cnt: # 아랫부분 통합
+                        return True
     
     return False
 
@@ -36,8 +33,6 @@ for i in range(TC):
     # 자기자신으로 돌아오고자 할 때, 음수가 되는 경우를 찾는다.
     # -> 벨만포드에서 음수 사이클이 발생하면 음수가 됨.
     # 근데 시작지점이 지정되어있지 않은데 모든 정점에 대해 검사하면 시간초과 안뜨나?
-    backward = False
-    for i in range(1, point_cnt + 1):
-        backward = backward or Bell(graph, i)
+    backward = Bell(graph, i)
 
     print("YES" if backward else "NO")
