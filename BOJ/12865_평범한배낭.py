a1971d65297f2d14d53f3802a666b7a341dbc1ac
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-09-22 23:21:21 +0900

    add comment

--- BOJ/12865_평범한배낭.py
+++ BOJ/12865_평범한배낭.py
@@ -13,6 +13,8 @@ DP = [0] * (max_weight + 1)
 
 for weight, value in things:
     for i in range(max_weight, weight - 1, -1):
+        # 순서를 신경써줘야 했던 이유 : 증가하는 방향인 경우, 참조해야 하는 값이 이전 loop에 의해 변경되므로,
+        # 뒤에서부터 확인을 해야 문제가 발생하지 않음.
         DP[i] = max(DP[i], DP[i - weight] + value)
     
 print(DP[-1])
