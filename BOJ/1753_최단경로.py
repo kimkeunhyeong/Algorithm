f615f634f117c6351a2bbf5804e3333e7a15a250
kimkeunhyeong <113564875+kimkeunhyeong@users.noreply.github.com>
2023-11-28 03:42:22 +0900

    [EDIT] change order of tuple element BOJ-1753, T : 656 ms, M : 70300 KB

--- BOJ/1753_최단경로.py
+++ BOJ/1753_최단경로.py
@@ -8,7 +8,7 @@ Vertex, Edge = map(int, input().split())
 graph = [[] for i in range(Vertex + 1)]
 
 start = int(input())
-Heap = [(start, 0)]
+Heap = [(0, start)]
 
 distance = [inf] * (Vertex + 1)
 distance[start] = 0
@@ -19,14 +19,17 @@ for i in range(Edge):
     
 # 다익스트라
 while Heap:
-    check_node, check_dist = heappop(Heap)
+    check_dist, check_node = heappop(Heap)
     if check_dist > distance[check_node]:
         continue
     for node, node_weight in graph[check_node]:
         new_weight = check_dist + node_weight
         if distance[node] > new_weight:
             distance[node] = new_weight
-            heappush(Heap, (node, new_weight))
+            # graph의 원소 순서랑 tuple의 원소 순서를 맞춰줬더니 heapQ가 제대로 작동하지 않아 시간초과가 발생함.
+            # tuple은 1순위로 전자를 비교하고, 그 값이 같은 경우 후순위를 비교함에 유의!!
+            # -> tuple 비교가 제대로 되지 않아 쓸데없는 update가 많이 발생하는 결과 초래.
+            heappush(Heap, (new_weight, node))
 
 for i in distance[1:]:
     if i != inf:
